1541481f76c2207c62d974494c14b0968375d293
seongEun95 <dsa02070@naver.com>
2025-08-24 23:47:01 +0900

    feat: 조회 기능 및 수정, 삭제 기능 추가

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,8 +2,32 @@ import Product from "../models/product.js";
 
 const getProducts = async (req, res, next) => {
   try {
-    const products = await Product.find();
-    res.status(200).json(products);
+    const { page = 1, limit = 10, sort, search } = req.query;
+    const skip = (page - 1) * limit;
+    const sortOption = { createdAt: sort === "recent" ? -1 : 1 };
+    const searchQuery = search
+      ? {
+          $or: [{ name: { $regex: search, $options: "i" } }, { description: { $regex: search, $options: "i" } }],
+        }
+      : {};
+
+    const sortedProducts = await Product.find(searchQuery)
+      .sort(sortOption)
+      .limit(limit)
+      .skip(skip)
+      .select("_id name price createdAt");
+
+    res.status(200).json(sortedProducts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getProductById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id).select("_id name description price tags createdAt");
+    res.status(200).json(product);
   } catch (error) {
     next(error);
   }
@@ -18,8 +42,37 @@ const createProduct = async (req, res, next) => {
       price,
       tags,
     });
-    console.log(newProduct);
     res.status(201).json(newProduct);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price, tags } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        name,
+        description,
+        price,
+        tags,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
@@ -27,7 +80,10 @@ const createProduct = async (req, res, next) => {
 
 const productController = {
   getProducts,
+  getProductById,
   createProduct,
+  updateProduct,
+  deleteProduct,
 };
 
 export default productController;
--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -5,7 +5,16 @@ const router = Router();
 // 상품 목록 조회
 router.get("/list", productController.getProducts);
 
+// 상품 상세 조회
+router.get("/detail/:id", productController.getProductById);
+
 // 상품 등록
 router.post("/create", productController.createProduct);
+
+// 상품 수정
+router.patch("/update/:id", productController.updateProduct);
+
+// 상품 삭제
+router.delete("/delete/:id", productController.deleteProduct);
 
 export default router;
