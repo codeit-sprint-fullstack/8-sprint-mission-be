b5e2997cc61f9b1aa7431e73a28cdd729af23835
chaewon127 <osobi621@gmail.com>
2025-08-31 22:32:30 +0900

    asyncHandler 함수 적용

--- app.js
+++ app.js
@@ -8,59 +8,90 @@ import mockComments from "./data/mockComments.js";
 const app = express();
 app.use(express.json());
 
-app.get("/mockArticles", async (req, res) => {
-  /**
-   * 쿼리 파라미터
-   * - sort: newest인 경우 새로운 게시글 기준, 아닌 경우 best?순
-   * - count: 게시글 개수
-   */
-  const sort = req.query.sort;
-  const count = Number(req.query.count) || 0;
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "ValidationError") {
+        res.status(400).send({ message: e.message });
+      } else if (e.name === "CastError") {
+        res.status(400).send({ message: "Cannot find given id" });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
 
-  const sortOptions = { createdAt: sort === "newest" ? "asc" : "desc" };
-  const newArticles = await Article.find().sort(sortOptions).limit(count);
+app.get(
+  "/mockArticles",
+  asyncHandler(async (req, res) => {
+    /**
+     * 쿼리 파라미터
+     * - sort: newest인 경우 새로운 게시글 기준, 아닌 경우 best?순
+     * - count: 게시글 개수
+     */
+    const sort = req.query.sort;
+    const count = Number(req.query.count) || 0;
 
-  res.send(newArticles);
-});
+    const sortOptions = { createdAt: sort === "newest" ? "asc" : "desc" };
+    const newArticles = await Article.find().sort(sortOptions).limit(count);
 
-app.get("/mockArticles/:id", async (req, res) => {
-  const id = req.params.id;
-  const Article = await Article.findById(id);
-  if (Article) {
-    res.send(Article);
-  } else {
-    res.status(404).send({ message: "Cannot find given id" });
-  }
-});
+    res.send(newArticles);
+  })
+);
 
-app.post("/mockArticles", async (req, res) => {
-  const newArticle = await Article.create(req.body);
-  res.status(201).send(newArticle);
-});
+app.get(
+  "/mockArticles/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const Article = await Article.findById(id);
+    if (Article) {
+      res.send(Article);
+    } else {
+      res.status(404).send({ message: "Cannot find given id" });
+    }
+  })
+);
 
-app.patch("/mockArticles/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const Article = mockArticles.find((Article) => Article.id === id);
-  if (Article) {
-    Object.keys(req.body).forEach((key) => {
-      Article[key] = req.body[key];
-    });
-  } else {
-    res.status(404).send({ message: "Cannot find given id" });
-    Article.updatedAt = new Date();
-    res.send(Article);
-  }
-});
+app.post(
+  "/mockArticles",
+  asyncHandler(async (req, res) => {
+    const newArticle = await Article.create(req.body);
+    res.status(201).send(newArticle);
+  })
+);
 
-app.delete("/mockArticles/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const idx = mockArticles.findIndex((Article) => Article.id === id);
-  if (idx >= 0) {
-    mockArticles.splice(idx, 1);
-    res.sendStatus(204);
-  } else {
-    res.status(404).send({ message: "Cannot find given id" });
-  }
-});
+app.patch(
+  "/mockArticles/:id",
+  asyncHandler(async (req, res) => {
+    const id = Number(req.params.id);
+    const Article = mockArticles.find((Article) => Article.id === id);
+    if (Article) {
+      Object.keys(req.body).forEach((key) => {
+        Article[key] = req.body[key];
+      });
+    } else {
+      res.status(404).send({ message: "Cannot find given id" });
+      Article.updatedAt = new Date();
+      res.send(Article);
+    }
+  })
+);
+
+app.delete(
+  "/mockArticles/:id",
+  asyncHandler(async (req, res) => {
+    const id = Number(req.params.id);
+    const idx = mockArticles.findIndex((Article) => Article.id === id);
+    if (idx >= 0) {
+      mockArticles.splice(idx, 1);
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ message: "Cannot find given id" });
+    }
+  })
+);
 
 app.listen(3000, () => console.log("Server Started"));
