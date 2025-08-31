539cf1f0d5d9c7b63255ffd70f87657b7e7f61ec
chaewon127 <osobi621@gmail.com>
2025-08-31 21:08:18 +0900

    mock 데이터 삽입

---
+++ data/mockComment.js
@@ -0,0 +1,34 @@
+const Commentdata = [
+  {
+    id: 1,
+    content: "저도 그 카페 가봤는데 정말 좋더라고요!",
+    createdAt: "2025-08-31T12:00:00Z",
+    articleId: 4,
+  },
+  {
+    id: 2,
+    content: "리액트 관련해서 유튜브 강의 추천드립니다.",
+    createdAt: "2025-08-30T16:00:00Z",
+    articleId: 2,
+  },
+  {
+    id: 3,
+    content: "주말에는 신과 함께 시리즈 추천!",
+    createdAt: "2025-08-29T19:00:00Z",
+    articleId: 3,
+  },
+  {
+    id: 4,
+    content: "날씨 진짜 좋네요, 산책 가야겠습니다.",
+    createdAt: "2025-08-31T10:30:00Z",
+    articleId: 1,
+  },
+  {
+    id: 5,
+    content: "BeautifulSoup 예제 코드 GitHub에 많아요.",
+    createdAt: "2025-08-27T10:00:00Z",
+    articleId: 5,
+  },
+];
+
+export default Commentdata;
