434026f954c7f8d1d22e1b64aa9cd635a1fadc22
chaewon127 <osobi621@gmail.com>
2025-08-31 22:16:12 +0900

    Read API 수정

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import { DATABASE_URL } from "./env.js";
 import mockArticles from "./data/mockArticles.js";
+import Article from "./models/Task.js";
 import mockComments from "./data/mockComments.js";
 
 const app = express();
 app.use(express.json());
 
-app.get("/mockArticles", (req, res) => {
+app.get("/mockArticles", async (req, res) => {
   /**
    * 쿼리 파라미터
    * - sort: newest인 경우 새로운 게시글 기준, 아닌 경우 best?순
    * - count: 게시글 개수
    */
   const sort = req.query.sort;
-  const count = Number(req.query.count);
+  const count = Number(req.query.count) || 0;
 
-  const compareFn =
-    sort === "newest"
-      ? (a, b) => b.createdAt - a.createdAt // 최신순
-      : (a, b) => a.createdAt - b.createdAt; // 오래된순
-
-  let newArticles = mockArticles.sort(compareFn);
-
-  if (count) {
-    newArticles = newArticles.slice(0, count);
-  }
+  const sortOptions = { createdAt: sort === "newest" ? "asc" : "desc" };
+  const newArticles = await Article.find().sort(sortOptions).limit(count);
 
   res.send(newArticles);
 });
 
-app.get("/mockArticles/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const Article = mockArticles.find((Article) => Article.id === id);
+app.get("/mockArticles/:id", async (req, res) => {
+  const id = req.params.id;
+  const Article = await Article.findById(id);
   if (Article) {
     res.send(Article);
   } else {
