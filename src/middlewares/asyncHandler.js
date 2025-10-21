88bdea89c4aa049186c2bfea6d326a62e466c059
chaewon127 <osobi621@gmail.com>
2025-10-21 18:37:01 +0900

    asyncHandler 및 errorhandler 작성

--- src/middlewares/asyncHandler.js
+++ src/middlewares/asyncHandler.js
@@ -1,21 +1,2 @@
-function asyncHandler(handler) {
-  return async function (req, res) {
-    try {
-      await handler(req, res);
-    } catch (e) {
-      if (
-        e.name === "StructError" ||
-        e instanceof Prisma.PrismaClientValidationError
-      ) {
-        res.status(400).send({ message: e.message });
-      } else if (
-        e instanceof Prisma.PrismaClientKnownRequestError &&
-        e.code === "P2025"
-      ) {
-        res.status(404).send({ message: "Cannot find given id" });
-      } else {
-        res.status(500).send({ message: e.message });
-      }
-    }
-  };
-}
+export const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+export default function errorHandler(e, req, res, next) {
+  console.error(e);
+  if (e.name === "StructError") {
+    return res.status(400).json({ message: e.message });
+  }
+  if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2025") {
+    return res.status(404).json({ message: "Cannot find given id" });
+  }
+  if (e.status && e.message) {
+    return res.status(e.status).json({ message: e.message });
+  }
+  res.status(500).json({ message: "Server error", detail: e.message });
+}
