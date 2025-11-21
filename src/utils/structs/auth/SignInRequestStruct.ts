2c07a124b6f22c62172c16c23fb55f89b0bce9cc
cuckooolly <cuckooolly@gmail.com>
2025-11-22 00:10:23 +0900

    refactor: Rename 'limit' to 'take' in GetCommentListRequestStruct for clarity and improve email validation in SignIn and SignUp request structures

--- src/utils/structs/auth/SignInRequestStruct.ts
+++ src/utils/structs/auth/SignInRequestStruct.ts
@@ -2,6 +2,8 @@ import { object, nonempty, string, define } from 'superstruct';
 import isEmail from 'is-email';
 
 export const SignInRequestStruct = object({
-    email: define('Email', isEmail),
+    email: define('Email', (value): value is string =>
+        typeof value === 'string' && isEmail(value)
+    ),
     password: nonempty(string()),
 });
--- src/utils/structs/auth/SignUpRequestStruct.ts
+++ src/utils/structs/auth/SignUpRequestStruct.ts
@@ -2,7 +2,9 @@ import { coerce, object, nonempty, string, define } from 'superstruct';
 import isEmail from 'is-email';
 
 export const SignUpRequestStruct = object({
-    email: define('Email', isEmail),
+    email: define('Email', (value): value is string =>
+        typeof value === 'string' && isEmail(value)
+    ),
     nickname: coerce(nonempty(string()), string(), (value) => value.trim()),
     password: nonempty(string()),
     passwordConfirmation: nonempty(string()),
