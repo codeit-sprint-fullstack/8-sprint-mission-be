ebbcfeacbc8fb52abcb8bd0adcab35a3307148db
MinJun916 <ed125248@gmail.com>
2025-11-26 19:15:31 +0900

    feat: complete /auth/me logic migration and refactoring

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,6 +4,8 @@ import { SigninSchemaType, SignupSchemaType } from '../validators/auth.validator
 import { signinService, signupService } from '../services/auth.service';
 import HTTP_STATUS from '../constants/http.constant';
 import COOKIE_OPTIONS from '../config/cookie';
+import { findUserById } from '../repositories/auth.repository';
+import { filterSensitiveData } from '../utils/filter';
 
 export const signupController = asyncHandler(
   async (req: Request<{}, {}, Omit<SignupSchemaType, 'passwordConfirm'>>, res: Response) => {
@@ -43,3 +45,33 @@ export const signinController = asyncHandler(
     });
   },
 );
+
+export const getMyInfoController = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.auth || !req.auth.userId) {
+    res.status(HTTP_STATUS.UNAUTHORIZED).json({
+      success: false,
+      message: '인증되지 않은 접근입니다.',
+    });
+    return;
+  }
+
+  const { userId } = req.auth;
+
+  const user = await findUserById(userId);
+
+  if (!user) {
+    res.status(HTTP_STATUS.NOT_FOUND).json({
+      success: false,
+      message: '존재하지 않는 사용자입니다.',
+    });
+    return;
+  }
+
+  res.status(HTTP_STATUS.OK).json({
+    success: true,
+    message: '사용자 정보를 조회했습니다.',
+    data: {
+      user: filterSensitiveData(user),
+    },
+  });
+});
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,17 @@
 import Router from 'express';
-import { signinController, signupController } from '../controllers/auth.controller';
+import {
+  getMyInfoController,
+  signinController,
+  signupController,
+} from '../controllers/auth.controller';
 import { validate } from '../middlewares/validator.middleware';
 import { signinSchema, signupSchema } from '../validators/auth.validator';
+import { verifyAccessToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/me');
+// TODO: 로그아웃 로직 추가
+router.get('/me', verifyAccessToken, getMyInfoController);
 router.post('/signup', validate(signupSchema), signupController);
 router.post('/signin', validate(signinSchema), signinController);
 
