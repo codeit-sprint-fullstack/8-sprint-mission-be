2e2356e019d0e97601e92a0be23cad18cd70eabd
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-11-30 23:37:00 +0900

    임시 제출

--- config/db.js
+++ config/db.js
@@ -1,3 +1,7 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient({
+  // prisma 7부터 직접 DB 연결
+  //   adapter: "postgresql",
+  //   url: process.env.DATABASE_URL,
+});
 export default prisma;
